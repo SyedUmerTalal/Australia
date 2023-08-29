c3b3016e4dcfeb19d4ffeee89ddfc60ddb164f30
SyedUmerTalal <umerbukhari222.ub@gmail.com>
2023-08-29 13:20:40 +0500

    City and states for Australia

---
+++ states.sql
@@ -0,0 +1,15 @@
+/*
+SQLyog Community
+MySQL - 8.0.33 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+insert into `states` (`state_id`, `state_name`) values('3907','Australian Capital Territory');
+insert into `states` (`state_id`, `state_name`) values('3909','New South Wales');
+insert into `states` (`state_id`, `state_name`) values('3910','Northern Territory');
+insert into `states` (`state_id`, `state_name`) values('3905','Queensland');
+insert into `states` (`state_id`, `state_name`) values('3904','South Australia');
+insert into `states` (`state_id`, `state_name`) values('3908','Tasmania');
+insert into `states` (`state_id`, `state_name`) values('3903','Victoria');
+insert into `states` (`state_id`, `state_name`) values('3906','Western Australia');
